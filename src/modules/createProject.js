6bc9d60f53a61e4e387ce8107deb766987bf023f
haycon <46656475+haycon@users.noreply.github.com>
2021-03-26 17:01:52 +0100

    Added checked functionality via js

--- src/modules/createProject.js
+++ src/modules/createProject.js
@@ -6,12 +6,8 @@ function createProject() {
   const div = document.createElement('div');
   const h2 = document.createElement('h2');
   const labelProjectName = document.createElement('label');
-  const labelDesc = document.createElement('label');
-  const labelCheckbox = document.createElement('label');
   const form = document.createElement('form');
   const inputProjectName = document.createElement('input');
-  const inputDesc = document.createElement('input');
-  const inputCheckbox = document.createElement('input');
   const br = document.createElement('br');
   const br1 = document.createElement('br');
   const br2 = document.createElement('br');
@@ -20,12 +16,7 @@ function createProject() {
   div.id = 'createTodo';
   h2.innerHTML = 'Create a new project';
   inputProjectName.id = 'inputProjectName';
-  inputDesc.id = 'inputDescriptionName';
-  inputCheckbox.id = 'inputCheckbox';
   labelProjectName.innerHTML = 'Project name: ';
-  labelDesc.innerHTML = 'Description: ';
-  labelCheckbox.innerHTML = 'Done (Yes/No): ';
-  inputCheckbox.type = 'checkbox';
   button.innerHTML = 'Create';
   button.addEventListener('click', addTodo);
 
@@ -35,12 +26,6 @@ function createProject() {
   form.appendChild(labelProjectName);
   form.appendChild(inputProjectName);
   form.appendChild(br);
-  form.appendChild(labelDesc);
-  form.appendChild(inputDesc);
-  form.appendChild(br2);
-  form.appendChild(labelCheckbox);
-  form.appendChild(inputCheckbox);
-  form.appendChild(br1);
   form.appendChild(button);
 
   return div;
@@ -58,10 +43,7 @@ function addTodo() {
   //It is a form, and therefore the page get's refreshed
   event.preventDefault();
   let projectName = document.getElementById('inputProjectName').value;
-  let description = document.getElementById('inputDescriptionName').value;
-  let checked = document.getElementById('inputCheckbox').checked;
-  let newTodo = Todo(description, checked);
-  let newProject = Project(projectName, newTodo);
+  let newProject = Project(projectName, null);
   projects.push(newProject);
 
   displayProjects();
@@ -72,22 +54,17 @@ function addTodo() {
   const h2 = document.createElement('h2');
   const p = document.createElement('p');
   const p2 = document.createElement('p');
-  const labelProjectName = document.createElement('label');
   const labelDesc = document.createElement('label');
   const labelCheckbox = document.createElement('label');
   const inputDesc = document.createElement('input');
   const inputCheckbox = document.createElement('input');
-  const checkbox = document.createElement('input');
-  checkbox.type = 'checkbox';
   const br = document.createElement('br');
   const br2 = document.createElement('br');
   const button = document.createElement('button');
 
   div.id = 'newTodo';
   div2.id = 'displayTodos';
-  p.classList.add('descriptionStyle');
   h2.innerHTML = projectName;
-  p.innerHTML = description;
   p2.innerHTML = 'Add new todos:';
   inputDesc.id = 'inputDesc';
   labelDesc.innerHTML = 'Description: ';
@@ -100,7 +77,6 @@ function addTodo() {
   rightBar.appendChild(div);
   div.appendChild(div2);
   div2.appendChild(h2);
-  div2.appendChild(checkbox);
   div2.appendChild(p);
   div.appendChild(p2);
   div.appendChild(labelDesc);
@@ -113,19 +89,23 @@ function addTodo() {
 }
 
 function addTodos() {
-  /*
-    inputProjectName.id = 'inputProjectName';
-    let inputDesc.id = 'inputDescriptionName';
-    let inputCheckbox.id = 'inputCheckbox';
-  */
-
+  let n = 0;
   let p = document.createElement('p');
   let checkbox = document.createElement('input');
   let br = document.createElement('br');
 
+  checkbox.classList.add('checkbox');
   checkbox.type = 'checkbox';
   p.innerHTML = inputDesc.value;
   p.classList.add('descriptionStyle');
+
+  checkbox.addEventListener('click', function (e) {
+    if (e.srcElement.checked == true) {
+      p.classList.add('completed');
+    } else {
+      p.classList.add('unfinished');
+    }
+  });
 
   displayTodos.appendChild(br);
   displayTodos.appendChild(checkbox);
@@ -134,6 +114,6 @@ function addTodos() {
   let newTodo = Todo(inputDesc.value, inputCheckbox.checked);
   todos.push(newTodo);
   console.log(todos);
+  n += 3;
 }
-
 export { createProject };
